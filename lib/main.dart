a1f6b18cb9af60082a9b35dd12fd43c49baf9cf4
unknown <anyfuture@protonmail.com>
2025-07-05 14:16:43 +0300

    Input style correction

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fv_chat/common/styles/app_colors.dart';
 import 'package:fv_chat/pages/chat_page.dart';
 
 void main() {
@@ -10,8 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ChatPage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: AppColors.neon,
+          selectionColor: AppColors.neonSade,
+          selectionHandleColor: AppColors.neon,
+        ),
+      ),
+      home: const ChatPage(),
     );
   }
 }
